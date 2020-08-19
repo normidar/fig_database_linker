ced4fac0f1c50bcb18fa6d319d90a1918009b4fe
normidar <1106408182@qq.com>
2020-08-19 11:42:02 +0900

    u

--- test/fig_database_linker_test.dart
+++ test/fig_database_linker_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fig_database_linker/dataLinkerPostgres.dart';
+import 'package:fig_database_linker/fig_database_linker.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
